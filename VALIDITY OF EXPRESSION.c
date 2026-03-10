8be2b80266c2fa0c784fad9513c87ccf80fbc9a2
Puja Biswas <pujabiswas33943@gmail.com>
2026-03-11 01:03:25 +0600

    Add files via upload

---
+++ VALIDITY OF EXPRESSION.c
@@ -0,0 +1,103 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#define MAX 100
+int top=-1;
+char stack[MAX];
+void push(char);char pop();
+int isFull();
+int isEmpty();int check_balanced(char*);
+int match_char(char,char);
+int main(){
+  char expr[MAX];
+  int balanced;
+  printf("Enter the algebraic expression:");
+  gets(expr);
+  balanced=check_balanced(expr);
+  if(balanced==1)
+    printf("The expression is valid.");
+  else
+   printf("The expression is not valid.");
+   return 0;
+}
+int check_balanced(char *s){
+char temp;
+int i;
+for(i=0;i<strlen(s);i++)
+{
+    if(s[i]=='['|| s[i]=='{' || s[i]=='(')
+        push(s[i]);
+      if(s[i]==']'|| s[i]=='}' || s[i]==')')
+      {
+          if(isEmpty()){
+            printf("Right brackets are more than left brackets.");
+            return 0;
+
+          }
+          else{
+            temp=pop();
+            if(!match_char(temp,s[i])){
+                printf("Mismatched bracket.\n");
+            return 0;
+            }
+          }
+      }
+}
+if(isEmpty()){
+
+    printf("Bracket are well balanced.\n");
+    return 1;
+}
+else{
+     printf("Left brackets are more than left brackets.");
+            return 0;
+}
+
+}
+void push(char c)
+{
+    if(isFull())
+       {
+           printf("Stack Overflow.");
+            exit(1);
+       }
+    top++;
+    stack[top]=c;
+}
+char pop(){
+    int c;
+if(isEmpty()){
+    printf("Stack Underflow.");
+    exit(1);
+}
+c=stack[top];
+top--;
+return c;
+
+}
+
+int isEmpty(){
+ if(top==-1)
+    return 1;else
+    return 0;
+}
+int isFull()
+{
+    if(top==MAX-1)
+        return 1;
+    else
+        return 0;
+}
+
+int match_char(char a,char b)
+{
+    if(a=='(' && b==')')
+        return 1;
+    if(a=='{' && b=='}')
+        return 1;
+        if(a=='[' && b==']')
+        return 1;
+    else
+        return 0;
+
+}
